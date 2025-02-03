b08af1a1726f2a5f4c47fc62320b92ce8cfaabf5
vishvastav <vishal.logicalthinker@gmail.com>
2025-02-03 22:47:10 +0530

    Not yet completed

--- src/Task/CountVowelsAndConsonants.java
+++ src/Task/CountVowelsAndConsonants.java
@@ -1,7 +1,20 @@
 package Task;
 
+import java.util.Scanner;
+
 public class CountVowelsAndConsonants {
     public static void main(String[] args) {
         //Count vowels and consonants in a String.  -pramod, → vowels - 2, consnants - 4
+//        String name = args[0];
+//        System.out.println("Enter your Name: " + args[0]);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string: ");
+        String countvowel = sc.next();
+
+        countvowel = countvowel.toLowerCase();
+
+        System.out.println(countvowel);
+
+
     }
 }
