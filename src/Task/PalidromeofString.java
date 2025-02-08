7b95ed8c848e01a9779a74d09b8880d9fbea7a8b
vishvastav <vishal.logicalthinker@gmail.com>
2025-02-08 10:36:19 +0530

    String function 04 Feb

--- src/Task/PalidromeofString.java
+++ src/Task/PalidromeofString.java
@@ -9,5 +9,15 @@ public class PalidromeofString {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter word to convert into palidrome: ");
         String palidrome = scanner.next();
+        StringBuffer sb = new StringBuffer(palidrome);
+        sb.reverse();
+        System.out.println("Your reversed word is " + sb);
+
+        String reversed = sb.toString();
+
+        if (palidrome.equals( reversed)) {
+            System.out.println(" YOUR INPUT IS A PALIDROME ");
+
+        }else System.out.println("YOUR INPUT IS NOT A PALIDROME");
     }
 }
